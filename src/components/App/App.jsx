dfcc4387e096cfbd4277db247db4a8f19a8d7ae8
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-09-11 11:09:15 +0300

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import Loyaut from "../Loyaut/Loyaut.jsx";
-import MainPage from "../../pages/MainPage/MainPage.jsx";
-import CarsPage from "../../pages/CarsPage/CarsPage.jsx";
-import CarsPageById from "../../pages/CarsPageById/CarsPageById.jsx";
+// import MainPage from "../../pages/MainPage/MainPage.jsx";
+// import CarsPage from "../../pages/CarsPage/CarsPage.jsx";
+// import CarsPageById from "../../pages/CarsPageById/CarsPageById.jsx";
+import { lazy, Suspense } from "react";
+
+const MainPage = lazy(() => import("../../pages/MainPage/MainPage.jsx"));
+const CarsPage = lazy(() => import("../../pages/CarsPage/CarsPage.jsx"));
+const CarsPageById = lazy(() =>
+  import("../../pages/CarsPageById/CarsPageById.jsx")
+);
 
 export default function App() {
   return (
     <Loyaut>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/catalog" element={<CarsPage />} />
-        <Route path="/cars/:id" element={<CarsPageById />} />
-      </Routes>
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/catalog" element={<CarsPage />} />
+          <Route path="/cars/:id" element={<CarsPageById />} />
+        </Routes>
+      </Suspense>
     </Loyaut>
   );
 }
